8167299772979bb3ea8f3a30a7fd2931eea4cca1
mehriafaqirzai-pvcc <148817923+mehriafaqirzai-pvcc@users.noreply.github.com>
2023-11-15 16:33:05 -0500

    Add files via upload

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,4 @@
-# Name: your-name-here
+# Name: Mehria Faqirzai
 # Prog Purpose: Hello World
 
 print("Hello world! This is my first Python program.")
@@ -6,3 +6,4 @@ print("I can program in HTML, which is awesome.")
 print("However, HTML cannot do arithmetic.")
 print("But Python can:\n\tAdd\n\tSubtract\n\tMultiply\n\tDivide")
 print("and lots of other things!")
+print("My name is Mehria Faqirzai")
