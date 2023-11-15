e801f8bc26fa13dce23921a559ad88da5d6174ce
mehriafaqirzai-pvcc <148817923+mehriafaqirzai-pvcc@users.noreply.github.com>
2023-11-15 16:23:34 -0500

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+# Name: your-name-here
+# Prog Purpose: Hello World
+
+print("Hello world! This is my first Python program.")
+print("I can program in HTML, which is awesome.")
+print("However, HTML cannot do arithmetic.")
+print("But Python can:\n\tAdd\n\tSubtract\n\tMultiply\n\tDivide")
+print("and lots of other things!")
